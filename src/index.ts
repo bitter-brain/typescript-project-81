d5b1553c01e4421546971f83f932061d64a8615e
Boris Alekseev <borisalekseev95@gmail.com>
2026-04-10 09:50:45 +0200

    export HexletCode from index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-import './tag'
+import Tag from './tag.js'
+
+const HexletCode = {
+  formFor: (template: Record<string, string>, options: Record<string, string> = {}, callback?: (f: any) => void) => {
+
+  },
+}
+
+export default HexletCode
