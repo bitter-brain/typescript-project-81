df343ecba58cf37a834cd25ad10e6a4bcd9c3028
Boris Alekseev <borisalekseev95@gmail.com>
2026-04-10 16:42:59 +0200

    implement formFor with empty form generation

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import Tag from './tag.js'
 
 const HexletCode = {
   formFor: (template: Record<string, string>, options: Record<string, string> = {}, callback?: (f: any) => void) => {
-
-  },
+    const action = options.url ?? '#'
+    const formAttrs = { action, method: 'post' }
+    return new Tag('form', formAttrs).toString()
+  }
 }
-
+const template = { name: 'rob', job: 'hexlet', gender: 'm' }
+console.log(HexletCode.formFor(template, {}, (f) => {}))
 export default HexletCode
